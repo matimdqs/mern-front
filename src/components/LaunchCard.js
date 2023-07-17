a49009a1df89098ba13cec016033ff35ace114b0
Matias Velilla <matiasvelillamdq@gmail.com>
2023-07-17 05:52:42 -0300

    even more tailwind adjustments. now moving to launch landing page

--- src/components/LaunchCard.js
+++ src/components/LaunchCard.js
@@ -18,13 +18,13 @@ const LaunchCard = ({ launch }) => {
             <div class="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
               <img src={launch.mission_patch} class="h-full w-full object-cover object-center" />
             </div>
-            <h3 class="mt-6 mb-3 text-sm text-gray-500">
+            <h3 class="mt-6 mb-1 text-base">
               <a href={`/launch/${launch.flight_number}`} target="_blank">
                 <span class="absolute inset-0"></span>
                 {launch.mission_name}
               </a>
             </h3>
-            <p class="text-sm py-2">{launch.details}</p>
+            <p class="text-sm py-2 text-gray-500">{launch.details}</p>
           </div>
           <form onSubmit={handleSubmit}>
             <button
